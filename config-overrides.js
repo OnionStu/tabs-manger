79286bd4466b77797cd8f003366d3d1aaedd4bf7
OnionStu <OnionSuig@gmail.com>
2018-07-16 02:00:48 +0800

    add rewrite

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,17 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const path = require('path');
+const { injectBabelPlugin, getLoader, loaderNameMatches } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
+const noHash = require('./lib/no-hash');
+const addEntry = require('./lib/add-entry');
+const rewireESLint = require('./lib/rewrite-eslint');
 const theme = require('./theme')();
 
 module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  console.log(config);
   console.log(env);
   config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-  config = rewireLess.withLoaderOptions({
-    javascriptEnabled: true,
-    modifyVars: theme
-  })(config, env);
+  config = rewireLess.withLoaderOptions({ javascriptEnabled: true, modifyVars: theme })(config, env);
+  config = rewireESLint.withLoaderOptions({ globals: { chrome: true } })(config, env);
+  config = addEntry({ background: path.resolve(__dirname, './src/background.js') })(config, env);
+  config = noHash(config, env);
   return config;
 };
---
+++ src/background.js
@@ -0,0 +1,34 @@
+console.log('Background start at %s', +new Date());
+let tabCount = 0;
+
+function getOpenTabs() {
+  return new Promise(resolve => {
+    chrome.tabs.query({ currentWindow: true }, tabs => {
+      resolve(tabs);
+    });
+  });
+}
+function setTabsCount(num) {
+  chrome.browserAction.setBadgeText({ text: String(num) });
+}
+getOpenTabs().then(tabs => {
+  tabCount = tabs.length;
+  console.log('open %s tabs', tabCount);
+  setTabsCount(tabCount);
+});
+
+chrome.tabs.onCreated.addListener(() => setTabsCount(++tabCount));
+chrome.tabs.onRemoved.addListener(() => setTabsCount(--tabCount));
+
+chrome.browserAction.onClicked.addListener(function() {
+  const index = chrome.extension.getURL('popup.html');
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    for (let i = 0, tab; (tab = tabs[i]); i++) {
+      if (tab.url && tab.url === index) {
+        chrome.tabs.update(tab.id, { selected: true });
+        return;
+      }
+    }
+    chrome.tabs.create({ url: index });
+  });
+});
