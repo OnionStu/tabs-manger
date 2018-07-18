bb20c1fea878de7788255960e79082ca4e19d630
OnionStu <OnionSuig@gmail.com>
2018-07-18 19:06:33 +0800

    添加截屏

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Layout, Card, Avatar, Icon, Row, Col } from 'antd';
 import classNames from 'classnames';
 
-import tabs from './tabs.json';
+import { sendMessage, updateTab, getTab } from './utils';
 import pic from './pic.png';
 import './app.less';
 
@@ -10,26 +10,65 @@ const { Header, Content } = Layout;
 const { Meta } = Card;
 
 class App extends Component {
+  state = { tabs: [] };
+
+  async componentDidMount() {
+    const { tabs, captures } = await sendMessage('getTabs');
+    tabs.forEach(tab => {
+      captures[tab.id] && (tab.capture = captures[tab.id]);
+    });
+    this.setState(() => ({ tabs }));
+  }
+
+  handlePushpin = async event => {
+    console.log('...');
+
+    const { dataset } = event.target;
+    console.log(dataset);
+
+    if (dataset && dataset.tabid) {
+      const tabId = +dataset.tabid;
+      const tab = await getTab(tabId);
+      const newTab = await updateTab(tabId, { pinned: !tab.pinned });
+      this.setState(prevState => {
+        const { tabs } = prevState;
+        tabs[dataset.index] = newTab;
+        return { tabs };
+      });
+    }
+  };
+
   render() {
-    const itemLayout = { md: 12, xl:8, xxl:6 }
+    const itemLayout = { md: 12, xl: 8, xxl: 6 },
+      { tabs } = this.state;
     return (
       <Layout className="main-layout">
-        <Header><h1>当前打开的标签：</h1></Header>
+        <Header>
+          <h1>当前打开的标签：</h1>
+        </Header>
         <Content>
           <Row gutter={20} type="flex" className="tab-list">
             {tabs &&
-              tabs.map(tab => {
-                const cardClass = classNames({'active-tab':tab.active})
-
+              tabs.map((tab, index) => {
+                const cardClass = classNames({ 'active-tab': tab.active });
+                const pushpinIcon = `pushpin${tab.pinned ? '' : '-o'}`;
                 return (
                   <Col key={tab.id} className="tab-item" {...itemLayout}>
                     <Card
                       className={cardClass}
                       bordered
-                      cover={<img src={pic} alt={tab.title} />}
-                      actions={[<Icon type="pushpin-o" />, <Icon type="reload" />, <Icon type="close" />]}
+                      cover={<img src={tab.capture} alt={tab.title} />}
+                      actions={[
+                        <Icon type={pushpinIcon} data-tabid={tab.id} data-index={index} onClick={this.handlePushpin} />,
+                        <Icon type="reload" />,
+                        <Icon type="close" />
+                      ]}
                     >
-                      <Meta title={tab.title} description={tab.url} avatar={<Avatar src={tab.favIconUrl} />} />
+                      <Meta
+                        title={tab.title}
+                        description={tab.url}
+                        avatar={tab.favIconUrl ? <Avatar src={tab.favIconUrl} /> : <Icon type="file" />}
+                      />
                     </Card>
                   </Col>
                 );
--- src/background.js
+++ src/background.js
@@ -1,5 +1,13 @@
+import { getTab } from './utils';
+
 console.log('Background start at %s', +new Date());
 let tabCount = 0;
+const captureMaps = {};
+
+const tabStatus = {
+  LOADING: 'loading',
+  COMPLETE: 'complete'
+};
 
 function getOpenTabs() {
   return new Promise(resolve => {
@@ -17,18 +25,57 @@ getOpenTabs().then(tabs => {
   setTabsCount(tabCount);
 });
 
-chrome.tabs.onCreated.addListener(() => setTabsCount(++tabCount));
-chrome.tabs.onRemoved.addListener(() => setTabsCount(--tabCount));
+async function dealWithGetTabs(params, sender) {
+  const tabs = await getOpenTabs();
+  console.log('send at %s', +new Date());
+  sender({ tabs, captures: captureMaps });
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.log('recive ', message);
+  if (message === 'getTabs') {
+    dealWithGetTabs(message, sendResponse);
+  }
+  return true;
+});
+
+chrome.tabs.onCreated.addListener(() => {
+  setTabsCount(++tabCount);
+});
+chrome.tabs.onRemoved.addListener(() => {
+  setTabsCount(--tabCount);
+});
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log(tabId, changeInfo, tab);
+  });
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const tab = await getTab(tabId);
+  console.log('onActivated...', tab);
+  if (!tab.url) return;
+  if (tab.url.startsWith('chrome://')) return;
+  if (tab.status !== tabStatus.COMPLETE) return;
+  chrome.tabs.captureVisibleTab(dataUrl => {
+    captureMaps[tabId] = dataUrl;
+  });
+});
 
 chrome.browserAction.onClicked.addListener(function() {
-  const index = chrome.extension.getURL('index.html');
-  chrome.tabs.getAllInWindow(undefined, function(tabs) {
-    for (let i = 0, tab; (tab = tabs[i]); i++) {
-      if (tab.url && tab.url === index) {
-        chrome.tabs.update(tab.id, { selected: true });
-        return;
+  console.log('on icon click');
+  try {
+    const index = chrome.extension.getURL('index.html');
+    chrome.tabs.getAllInWindow(undefined, function(tabs) {
+      console.log(tabs);
+      for (let i = 0, tab; (tab = tabs[i]); i++) {
+        console.log(tab, tab.url);
+        if (tab.url && tab.url === index) {
+          chrome.tabs.update(tab.id, { selected: true });
+          return;
+        }
       }
-    }
-    chrome.tabs.create({ url: index });
-  });
+      console.log('no one create one');
+      chrome.tabs.create({ url: index });
+    });
+  } catch (error) {
+    console.log('onClicked error', error);
+  }
 });
